722536996e89416750ea7addce3552384bc1f5b7
RAJ SR <rajsr.d.910@gmail.com>
2025-12-02 12:57:02 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using RSS.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RSS
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Form1 f1 = new Form1();
+            Application.Run(f1);
+        }
+    }
+}
